678cdaf57cd7778418bf3d769800b56c3f9c7924
assly <blue20will@gmail.com>
2018-07-30 16:57:20 +0900

    Step6: Move state management to Redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,15 @@
 /*global document */
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer, { DEFAULT_STATE } from './reducers';
 
-ReactDOM.render( < App / > , document.getElementById('root'));
+const store = createStore(rootReducer, DEFAULT_STATE);
+
+ReactDOM.render((
+  <Provider store={store}>
+    <App />
+  </Provider>
+), document.getElementById('root'));
