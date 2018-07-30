dc9dca5e2a440599e2cea2e9c325b15e73792cde
assly <blue20will@gmail.com>
2018-07-30 17:19:49 +0900

    Step7: Handle async with redux-saga

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,23 @@
-/*global document */
+/*global document window */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import rootReducer, { DEFAULT_STATE } from './reducers';
+import rootSaga from './sagas';
 
-const store = createStore(rootReducer, DEFAULT_STATE);
+const sagaMiddleware = createSagaMiddleware();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  DEFAULT_STATE,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render((
   <Provider store={store}>
